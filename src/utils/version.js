14fff850f5193abf0b3dbe49ffd0726f86ed3629
Pedro Ferreira <phsf.pedro@gmail.com>
2019-03-20 21:48:05 +0000

    feat: show testnet message agreement and indicative inside the wallet

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,6 +1,6 @@
 import versionApi from '../api/version';
 import store from '../store/index';
-import { isVersionAllowedUpdate } from '../actions/index';
+import { isVersionAllowedUpdate, networkUpdate } from '../actions/index';
 import { MIN_API_VERSION, FIRST_WALLET_COMPATIBLE_VERSION } from '../constants';
 import helpers from './helpers';
 import transaction from './transaction';
@@ -9,8 +9,12 @@ const version = {
   checkApiVersion() {
     const promise = new Promise((resolve, reject) => {
       versionApi.getVersion((data) => {
+        // Update version allowed in redux
         store.dispatch(isVersionAllowedUpdate({allowed: helpers.isVersionAllowed(data.version, MIN_API_VERSION)}));
+        // Update transaction weight constants
         transaction.updateTransactionWeightConstants(data.min_tx_weight, data.min_tx_weight_coefficient, data.min_tx_weight_k);
+        // Update network in redux
+        store.dispatch(networkUpdate({network: data.network}));
         resolve();
       }, (error) => {
         reject();
