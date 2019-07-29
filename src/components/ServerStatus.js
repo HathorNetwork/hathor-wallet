1f9b69adce0e15caf02b510ccf3352964865a179
Pedro Ferreira <pedro@hathor.network>
2019-07-30 00:15:20 +0200

    fix: sometimes we show the ServerStatus component and havent received… (#36)

--- src/components/ServerStatus.js
+++ src/components/ServerStatus.js
@@ -23,8 +23,11 @@ const mapStateToProps = (state) => {
  * @memberof Components
  */
 const ServerStatus = (props) => {
+  if (props.isOnline === undefined || !props.network) {
+    return null;
+  }
+
   return (
-    props.isOnline !== undefined && 
     <div className="d-flex flex-column version-wrapper align-items-center">
       <span className={props.network.startsWith("testnet") ? "text-testnet" : ""}>{props.network}</span>
       <span className={props.isOnline ? "" : "text-danger"}>{props.isOnline ? 'Online' : 'Offline'}</span>
