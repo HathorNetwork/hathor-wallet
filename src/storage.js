96c6b7f6a41cf7f16e9068ef672106175e07174b
Yan Martins <yan.martins@gmail.com>
2019-08-01 19:46:23 +0100

    chore: revert change to storage

--- src/storage.js
+++ src/storage.js
@@ -1,20 +1,6 @@
 class LocalStorageStore {
   getItem(key) {
-    let item;
-    try {
-      item = localStorage.getItem(key);
-      return JSON.parse(item);
-    } catch (e) {
-      // old versions of the wallet would save strings without converting
-      // to JSON, so we catch this exception here and return the string directly
-      if (e instanceof SyntaxError) {
-        // first save in JSON format
-        this.setItem(key, item);
-        // return it
-        return item;
-      }
-      throw e;
-    }
+    return JSON.parse(localStorage.getItem(key));
   }
 
   setItem(key, value) {
