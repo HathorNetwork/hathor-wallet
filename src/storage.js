96785adbe7a5791a58d375adb2496a0842ba3d3e
Yan Martins <yan.martins@gmail.com>
2019-08-08 16:57:28 +0100

    chore: add FIXME comment on storage

--- src/storage.js
+++ src/storage.js
@@ -7,6 +7,8 @@ class LocalStorageStore {
     } catch (e) {
       // old versions of the wallet would save strings without converting
       // to JSON, so we catch this exception here and return the string directly
+      // FIXME this is a temporary solution and should be fixed by versioning
+      // the storage: https://github.com/HathorNetwork/hathor-wallet-lib/issues/19
       if (e instanceof SyntaxError) {
         // first save in JSON format
         this.setItem(key, item);
