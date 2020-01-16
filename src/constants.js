016879d832dab425532a52505227401cbac9cc9f
Pedro Ferreira <pedro@hathor.network>
2020-01-16 18:13:58 -0300

    fix: removing wrong local test commit

--- src/constants.js
+++ src/constants.js
@@ -95,7 +95,6 @@ export const SENTRY_DSN = process.env.SENTRY_DSN || 'https://69c067d1587c465cac8
  * Server options for the user to choose which one to connect
  */
 export const DEFAULT_SERVERS = [
-  'http://localhost:8083/v1a/',
   'https://node1.mainnet.hathor.network/v1a/',
   'https://node2.mainnet.hathor.network/v1a/',
   'https://node3.mainnet.hathor.network/v1a/',
