5a84e48089ba1001d11d08558f38aa22c02509d5
Yan Martins <yan.martins@gmail.com>
2020-02-14 19:40:30 +0100

    chore: bump versions

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const WALLET_HISTORY_COUNT = 10;
 /**
  * Wallet version
  */
-export const VERSION = '0.13.0';
+export const VERSION = '0.13.1';
 
 /**
  * Before this version the data in localStorage from the wallet is not compatible  
