70b03631b2def4fcb60e316d402b9916fdf3b960
Pedro Ferreira <pedro@hathor.network>
2020-09-04 21:44:44 -0300

    chore: bump wallet version to v0.17.1 and lib to v0.16.2

--- public/electron.js
+++ public/electron.js
@@ -34,7 +34,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.17.0';
+const walletVersion = '0.17.1';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
