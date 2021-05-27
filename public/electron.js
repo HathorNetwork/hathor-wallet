111e5a6753cfd868a2805be96d092e3740aad4c2
Pedro Ferreira <pedro@hathor.network>
2021-05-27 11:38:30 -0300

    chore: updating version UI vars to v0.19.0 (#199)

--- public/electron.js
+++ public/electron.js
@@ -34,7 +34,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.18.2';
+const walletVersion = '0.19.0';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
