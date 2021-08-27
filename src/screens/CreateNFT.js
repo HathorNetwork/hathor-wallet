a3edcf9562cd6fde3074aa3c6424989e4f36cd94
Pedro Ferreira <pedro@hathor.network>
2021-08-27 15:12:21 -0300

    fix: translation variable should be calculated before the string

--- src/screens/CreateNFT.js
+++ src/screens/CreateNFT.js
@@ -234,6 +234,7 @@ class CreateNFT extends React.Component {
 
     const htrDeposit = hathorLib.tokens.getDepositPercentage() * 100;
     const depositAmount = hathorLib.tokens.getDepositAmount(this.state.amount); 
+    const nftFee = hathorLib.helpers.prettyValue(tokens.getNFTFee());
 
     return (
       <div className="content-wrapper">
@@ -244,7 +245,7 @@ class CreateNFT extends React.Component {
         <p>{t`Remember to make a backup of your new token's configuration string. You will need to send it to other people to allow them to use your NFT.`}</p>
         <p>
           {str2jsx(
-            t`When creating and minting NFTs, a |bold:deposit of ${htrDeposit}%| in HTR is required and an additional |bold:fee of ${hathorLib.helpers.prettyValue(tokens.getNFTFee())} HTR|. If these tokens are later melted, this HTR deposit will be returned (depending on the amount melted) and the fee can never be returned. Read more about the NFT standard |link:here|.`,
+            t`When creating and minting NFTs, a |bold:deposit of ${htrDeposit}%| in HTR is required and an additional |bold:fee of ${nftFee} HTR|. If these tokens are later melted, this HTR deposit will be returned (depending on the amount melted) and the fee can never be returned. Read more about the NFT standard |link:here|.`,
             {
               bold: (x, i) => <strong key={i}>{x}</strong>,
               link: (x, i) => <a key={i} href="true" onClick={this.goToRFC}>{x}</a>,
