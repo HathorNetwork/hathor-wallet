8f0be996aded4a494113b63ab092f784d24fdd39
André Abadesso <andre.abadesso@gmail.com>
2022-04-01 18:37:15 -0300

    feat: added wallet service constants

--- src/constants.js
+++ src/constants.js
@@ -219,3 +219,10 @@ export const LEDGER_MAX_VERSION = '2.0.0';
  * First Ledger version with support for custom tokens
  */
 export const LEDGER_FIRST_CUSTOM_TOKEN_COMPATIBLE_VERSION = '1.1.0'
+
+
+/**
+ * Wallet service URLs
+ */
+export const WALLET_SERVICE_MAINNET_BASE_URL = 'https://wallet-service.hathor.network/';
+export const WALLET_SERVICE_MAINNET_BASE_WS_URL = 'wss://ws.wallet-service.hathor.network/';
