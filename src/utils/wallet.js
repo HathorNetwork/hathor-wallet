e1a86822a2465c3dd2a9b091847d311eecf0df6b
André Abadesso <andre.abadesso@gmail.com>
2022-04-07 15:03:06 -0300

    chore: restore sentry

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -774,10 +774,10 @@ const wallet = {
    * @inner
    */
   initSentry(dsn) {
-    /*Sentry.init({
+    Sentry.init({
       dsn: dsn,
       release: process.env.npm_package_version
-    })*/
+    });
   },
 
   /**
@@ -789,7 +789,7 @@ const wallet = {
   updateSentryState() {
     if (this.isSentryAllowed()) {
       // Init Sentry
-      // this.initSentry(SENTRY_DSN);
+      this.initSentry(SENTRY_DSN);
     } else {
       // Turn off Sentry
       this.initSentry('');
