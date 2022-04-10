114e9b2eda6c045931fcf51988a2fe1323ca3d7b
André Abadesso <andre.abadesso@gmail.com>
2022-04-10 19:49:49 -0300

    feat: added a loading on the unlock button to prevent multiple clicks

--- src/screens/LockedWallet.js
+++ src/screens/LockedWallet.js
@@ -15,7 +15,7 @@ import RequestErrorModal from '../components/RequestError';
 import hathorLib from '@hathor/wallet-lib';
 import ReactLoading from 'react-loading';
 import { resolveLockWalletPromise } from '../actions';
-import colors from '../index.scss'
+import colors from '../index.scss';
 
 
 const mapStateToProps = (state) => {
