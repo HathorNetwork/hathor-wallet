2302b51e56e17d9601aa576cdc0b32ee5850592b
Tulio Miranda <tulio.mir@gmail.com>
2022-07-08 13:48:09 -0300

    revert: unrelated style fix

--- src/screens/CreateNFT.js
+++ src/screens/CreateNFT.js
@@ -257,7 +257,7 @@ class CreateNFT extends React.Component {
     // so this must be 0.01 (the text will show 0.01%) because the amount to create for NFTs is an integer.
     // Then to create 100 units, the deposit is 0.01 HTR, to create 1,000 units the deposit is 0.1 HTR
     const htrDeposit = hathorLib.tokens.getDepositPercentage();
-    const depositAmount = hathorLib.tokens.getDepositAmount(this.state.amount);
+    const depositAmount = hathorLib.tokens.getDepositAmount(this.state.amount); 
     const nftFee = hathorLib.helpers.prettyValue(tokens.getNFTFee());
 
     return (
