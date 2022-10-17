cb31f17e9e0730aed8c8f76a736081e4c805e4e3
André Abadesso <andre.abadesso@gmail.com>
2022-10-17 14:00:52 -0300

    fix: storing routerHistory on redux state

--- src/actions/index.js
+++ src/actions/index.js
@@ -293,6 +293,11 @@ export const walletReloadData = () => ({
   type: types.WALLET_RELOAD_DATA,
 });
 
+export const storeRouterHistory = (routerHistory) => ({
+  type: types.STORE_ROUTER_HISTORY,
+  routerHistory,
+});
+
 /**
  * @param {RouterHistory} routerHistory History object from react-dom-navigation
  */
