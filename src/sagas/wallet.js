9672e3e82db80d072e94f0a6435f74cff2970d08
André Abadesso <andre.abadesso@gmail.com>
2022-10-27 10:38:27 -0300

    chore: removed console.log

--- src/sagas/wallet.js
+++ src/sagas/wallet.js
@@ -547,7 +547,6 @@ export function* onWalletConnStateUpdate({ payload }) {
 }
 
 export function* walletReloading() {
-  console.log('Wallet reloading.');
   const wallet = yield select((state) => state.wallet);
   const routerHistory = yield select((state) => state.routerHistory);
 
