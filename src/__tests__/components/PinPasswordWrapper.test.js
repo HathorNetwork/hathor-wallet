3908138e051a8f478141a9481933ee8dfd82ea46
tuliomir <tulio.mir@gmail.com>
2022-10-28 19:07:16 -0300

    test: draft for testing user inputs with testing-library

--- src/__tests__/components/PinPasswordWrapper.test.js
+++ src/__tests__/components/PinPasswordWrapper.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
+import { render, act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import PinPasswordWrapper from '../../components/PinPasswordWrapper';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
+  render(
     <PinPasswordWrapper
       message={(<div/>)}
       success={jest.fn()}
@@ -16,3 +32,30 @@ it('renders without crashing', () => {
     />,
     div);
 });
+
+it('calls change handler method on change', async () => {
+  expect.assertions(2);
+
+  const changeHandler = jest.fn();
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={(<div/>)}
+        success={jest.fn()}
+        back={jest.fn()}
+        handleChange={changeHandler}
+        field={`Password`}
+        button={`Next`}
+        pattern={'\\'}
+      />,
+      container
+    )
+  });
+
+  // Get the element
+  const passInput = screen.getByPlaceholderText('Password');
+  await userEvent.type(passInput, 'abc123');
+  expect(changeHandler).toHaveBeenCalledTimes(6);
+  expect(changeHandler).toHaveBeenLastCalledWith('abc123')
+})
