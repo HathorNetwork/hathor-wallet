4911be3a36d38de71b44f574d71251f9ceb2015b
tuliomir <tulio.mir@gmail.com>
2022-10-31 09:57:09 -0300

    test: complete unit tests

--- src/__tests__/components/PinPasswordWrapper.test.js
+++ src/__tests__/components/PinPasswordWrapper.test.js
@@ -3,6 +3,7 @@ import { unmountComponentAtNode } from 'react-dom';
 import { render, act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import PinPasswordWrapper from '../../components/PinPasswordWrapper';
+import * as _ from 'lodash';
 
 let container = null;
 beforeEach(() => {
@@ -18,6 +19,7 @@ afterEach(() => {
   container = null;
 });
 
+// Rendering
 it('renders without crashing', () => {
   render(
     <PinPasswordWrapper
@@ -32,6 +34,83 @@ it('renders without crashing', () => {
     container);
 });
 
+it('renders with the specified placeholder text', () => {
+  expect.assertions();
+  const placeholderText = `field-${Math.random()}`;
+
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={(<div/>)}
+        success={jest.fn()}
+        back={jest.fn()}
+        handleChange={jest.fn()}
+        field={placeholderText}
+        button={`Next`}
+        pattern={'*'}
+      />,
+      container
+    )
+  });
+
+  // Get the element
+  const passInput = screen.getByPlaceholderText(placeholderText);
+  expect(passInput instanceof HTMLElement).toStrictEqual(true);
+});
+
+it('renders with the specified message', () => {
+  expect.assertions();
+  const message = `Sample message ${Math.random()}`;
+
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={<div>{message}</div>}
+        success={jest.fn()}
+        back={jest.fn()}
+        handleChange={jest.fn()}
+        field={'Password'}
+        button={`Next`}
+        pattern={'*'}
+      />,
+      container
+    )
+  });
+
+  // Get the element
+  const element = screen.getByText(message);
+  expect(element instanceof HTMLElement).toStrictEqual(true);
+});
+
+it('renders with the specified "next" button label', () => {
+  expect.assertions();
+  const message = `Btn ${Math.random()}`;
+
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={<div/>}
+        success={jest.fn()}
+        back={jest.fn()}
+        handleChange={jest.fn()}
+        field={'Password'}
+        button={message}
+        pattern={'*'}
+      />,
+      container
+    )
+  });
+
+  // Get the element
+  const button = screen.getByText(message);
+  expect(button instanceof HTMLElement).toStrictEqual(true);
+  expect(button.classList).toContain('btn-hathor')
+});
+
+// Methods
 it('calls change handler method on change', async () => {
   expect.assertions();
 
@@ -57,7 +136,33 @@ it('calls change handler method on change', async () => {
   await userEvent.type(passInput, 'abc123');
   expect(changeHandler).toHaveBeenCalledTimes(6);
   expect(changeHandler).toHaveBeenLastCalledWith('abc123')
-})
+});
+
+it('calls back handler method on button click', async () => {
+  expect.assertions();
+
+  const backHandler = jest.fn();
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={(<div/>)}
+        success={jest.fn()}
+        back={backHandler}
+        handleChange={jest.fn()}
+        field={`Password`}
+        button={`Next`}
+        pattern={'*'}
+      />,
+      container
+    )
+  });
+
+  // Get the element
+  const backButton = screen.getByText('Back');
+  await userEvent.click(backButton);
+  expect(backHandler).toHaveBeenCalledTimes(1);
+});
 
 it('ensures the field pattern is applied', async () => {
   expect.assertions();
@@ -94,4 +199,49 @@ it('ensures the field pattern is applied', async () => {
   await userEvent.type(passInput, passingText);
   expect(passInput.validity.patternMismatch).toStrictEqual(false);
   expect(passInput.checkValidity()).toStrictEqual(true);
+});
+
+it('calls success handler method on button click', async () => {
+  expect.assertions();
+
+  const successHandler = jest.fn();
+  // Render the element
+  act(() => {
+    render(
+      <PinPasswordWrapper
+        message={(<div/>)}
+        success={successHandler}
+        back={jest.fn()}
+        handleChange={jest.fn()}
+        field={`Password`}
+        button={`Next`}
+        pattern={'[0-9]+'} // Only numbers are allowed
+      />,
+      container
+    )
+  });
+
+  // Get the elements
+  const nextButton = screen.getByText('Next');
+  const passInput = screen.getByPlaceholderText('Password');
+  const confirmPassInput = screen.getByPlaceholderText(/^Confirm/);
+
+  // Fill with invalid data and click
+  await userEvent.type(passInput, 'abc');
+  await userEvent.click(nextButton);
+  expect(passInput.parentElement.classList).toContain('was-validated');
+  expect(successHandler).not.toHaveBeenCalled();
+
+  // Fill with correct data, but with invalid confirmation and click
+  await userEvent.clear(passInput);
+  await userEvent.type(passInput, '123');
+  await userEvent.click(nextButton);
+  expect(passInput.parentElement.classList).toContain('was-validated');
+  expect(successHandler).not.toHaveBeenCalled();
+
+  // Confirm with correct data and click
+  await userEvent.type(confirmPassInput, '123');
+  await userEvent.click(nextButton);
+  expect(passInput.parentElement.classList).not.toContain('was-validated');
+  expect(successHandler).toHaveBeenCalledTimes(1);
 })
