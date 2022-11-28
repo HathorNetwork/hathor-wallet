dcd9c58da9394707bb981453e4be53d2cc296bf7
tuliomir <tulio.mir@gmail.com>
2022-11-28 20:46:06 -0300

    test: basic interaction with app

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -4,5 +4,26 @@ describe('rendering the page', () => {
     cy.visit('/');
     cy.findByText(/Welcome to Hathor Wallet/i)
       .should("have.length", 1);
+  });
+
+  it('should only allow clicking the button after accepting terms', () => {
+    cy.visit('/');
+
+    // Click
+    const buttonLabel = 'Get started'
+    cy.findByText(buttonLabel).click();
+
+    // Recipe from: https://glebbahmutov.com/cypress-examples/8.7.0/recipes/form-validation.html
+    cy.get('form').then(
+      ($form) => expect($form[0].checkValidity()).to.be.false,
+    )
+
+    // Agree and click again
+    cy.get('#confirmAgree').click();
+    cy.findByText(buttonLabel).click();
+
+    // Should be on wallet selection screen
+    cy.contains('software');
+    cy.contains('hardware');
   })
 })
