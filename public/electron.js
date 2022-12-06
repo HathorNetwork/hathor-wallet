264cc48eef977284e38c270a48d74d73bf85251f
André Abadesso <andre.abadesso@gmail.com>
2022-12-06 13:13:52 -0300

    Revert "chore: bumped to v0.24.2"
    This reverts commit 8c00de3688157f578db70e0f981251130e4fc1e3.

--- public/electron.js
+++ public/electron.js
@@ -35,7 +35,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.24.2';
+const walletVersion = '0.24.1';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
