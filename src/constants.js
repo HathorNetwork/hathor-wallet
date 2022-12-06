7a61789fc464ea3a6f371837d3574e56af4f6a32
André Abadesso <andre.abadesso@gmail.com>
2022-12-06 19:15:34 -0300

    chore: bump v0.24.1 (#367)
    * chore: bumped to v0.24.1
    
    * chore: bumped to v0.24.2
    
    * Revert "chore: bumped to v0.24.2"
    
    This reverts commit 8c00de3688157f578db70e0f981251130e4fc1e3.

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const WALLET_HISTORY_COUNT = 10;
 /**
  * Wallet version
  */
-export const VERSION = '0.24.0';
+export const VERSION = '0.24.1';
 
 /**
  * Before this version the data in localStorage from the wallet is not compatible  
