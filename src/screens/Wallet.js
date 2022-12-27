f66b12496d1dab389933a5fa6789baa6b414b5cc
André Abadesso <andre.abadesso@gmail.com>
2022-12-27 13:37:07 -0300

    refactor/global token bar (#374)
    * refactor: TokenBar is now rendered on index.js and is a function component
    
    * Update src/components/TokenBar.js
    
    Co-authored-by: Pedro Ferreira <pedro@hathor.network>
    
    * docs: added a comment on TokenBar component
    
    Co-authored-by: Pedro Ferreira <pedro@hathor.network>

--- src/screens/Wallet.js
+++ src/screens/Wallet.js
@@ -16,7 +16,6 @@ import SpanFmt from '../components/SpanFmt';
 import WalletHistory from '../components/WalletHistory';
 import WalletBalance from '../components/WalletBalance';
 import WalletAddress from '../components/WalletAddress';
-import TokenBar from '../components/TokenBar';
 import TokenGeneralInfo from '../components/TokenGeneralInfo';
 import TokenAdministrative from '../components/TokenAdministrative';
 import HathorAlert from '../components/HathorAlert';
@@ -523,7 +522,6 @@ class Wallet extends React.Component {
           <BackButton {...this.props} />
           {renderUnlockedWallet()}
         </div>
-        <TokenBar {...this.props} />
         <HathorAlert ref={this.alertSuccessRef} text={this.state.successMessage} type="success" />
       </div>
     );
