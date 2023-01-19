3d7978f8c0e9534dd73c298151d663b3716068e7
André Abadesso <andre.abadesso@gmail.com>
2023-01-19 09:06:56 -0300

    chore: bumped to v0.25.0

--- public/electron.js
+++ public/electron.js
@@ -35,7 +35,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.24.1';
+const walletVersion = '0.25.0';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
