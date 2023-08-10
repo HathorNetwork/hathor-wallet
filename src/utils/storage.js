a52b5f02c11ecf1bb445328bcdc3d16277900638
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-08-10 11:07:52 -0300

    feat: clean unused keys

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -9,7 +9,7 @@ import LOCAL_STORE, { STARTED_KEY, LOCKED_KEY, CLOSED_KEY, IS_BACKUP_DONE_KEY, I
 
 const storageUtils = {
   migratePreviousLocalStorage() {
-    const accessData = localStorage.getItem('wallet:accessData');
+    const accessData = LOCAL_STORE.getItem('wallet:accessData');
 
     if (!accessData) {
       return;
@@ -20,30 +20,35 @@ const storageUtils = {
         STARTED_KEY,
         LOCAL_STORE.getItem('wallet:started'),
       );
+      LOCAL_STORE.removeItem('wallet:started');
     }
     if (LOCAL_STORE.getItem('wallet:locked')) {
       LOCAL_STORE.setItem(
         LOCKED_KEY,
         LOCAL_STORE.getItem('wallet:locked'),
       );
+      LOCAL_STORE.removeItem('wallet:locked');
     }
     if (LOCAL_STORE.getItem('wallet:closed')) {
       LOCAL_STORE.setItem(
         CLOSED_KEY,
         LOCAL_STORE.getItem('wallet:closed'),
       );
+      LOCAL_STORE.removeItem('wallet:closed');
     }
     if (LOCAL_STORE.getItem('wallet:backup')) {
       LOCAL_STORE.setItem(
         IS_BACKUP_DONE_KEY,
         LOCAL_STORE.getItem('wallet:backup'),
       );
+      LOCAL_STORE.removeItem('wallet:backup');
     }
     if (LOCAL_STORE.getItem('wallet:type')) {
       LOCAL_STORE.setItem(
         IS_HARDWARE_KEY,
         LOCAL_STORE.getItem('wallet:type') === 'hardware',
       );
+      LOCAL_STORE.removeItem('wallet:type');
     }
   }
 };
