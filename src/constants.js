38fee9ef11d7a8f25033acd4c2e3e425c72f65bb
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-10-16 17:15:42 -0300

    chore: bump version v0.27.0-rc4 (#419)

--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const WALLET_HISTORY_COUNT = 10;
 /**
  * Wallet version
  */
-export const VERSION = '0.27.0-rc3';
+export const VERSION = '0.27.0-rc4';
 
 /**
  * Before this version the data in localStorage from the wallet is not compatible
