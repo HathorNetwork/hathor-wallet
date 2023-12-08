00164fced8edfaed69e4620f3df65e64fafc8aa7
Ketan Sahu <rorozoro9888@gmail.com>
2023-12-08 22:05:07 +0530

    fix: automatically removes whitespaces
    Closes: #443
    Co-authored-by: Ketan Sahu <rorozoro9888@gmail.com>

--- src/components/SendTokensOne.js
+++ src/components/SendTokensOne.js
@@ -104,7 +104,7 @@ class SendTokensOne extends React.Component {
   getData = () => {
     let data = {'outputs': [], 'inputs': []};
     for (const output of this.outputs) {
-      const address = output.current.address.current.value;
+      const address = output.current.address.current.value.replace(/\s/g, '');
       const valueStr = (output.current.value.current.value || "").replace(/,/g, '');
 
       if (address && valueStr) {
