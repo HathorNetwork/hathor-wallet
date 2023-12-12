ed5426c945bac3a0d8a942bbcde8c1373e098a2b
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-12-12 15:20:31 -0300

    chore: bump version v0.27.1-rc1 (#454)

--- public/electron.js
+++ public/electron.js
@@ -35,7 +35,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.27.0';
+const walletVersion = '0.27.1-rc1';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
--- src/constants.js
+++ src/constants.js
@@ -20,7 +20,7 @@ export const WALLET_HISTORY_COUNT = 10;
 /**
  * Wallet version
  */
-export const VERSION = '0.27.0';
+export const VERSION = '0.27.1-rc1';
 
 /**
  * Before this version the data in localStorage from the wallet is not compatible
