cf12a0a2b8911f1e4a38ea2beec45e5504afbd5c
André Carneiro <andreluizmrcarneiro@gmail.com>
2024-01-09 16:01:24 -0500

    chore: bump v0.27.1-rc3

--- public/electron.js
+++ public/electron.js
@@ -35,7 +35,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.27.1-rc2';
+const walletVersion = '0.27.1-rc3';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
