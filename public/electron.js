e092304e83a60a8a2af35c63c197114ad34c6db6
tuliomir <tulio.mir@gmail.com>
2024-05-31 09:40:55 -0300

    chore: bump to v0.28.1 release

--- public/electron.js
+++ public/electron.js
@@ -35,7 +35,7 @@ if (process.platform === 'darwin') {
 }
 
 const appName = 'Hathor Wallet';
-const walletVersion = '0.28.1-rc.2';
+const walletVersion = '0.28.1';
 
 const debugMode = (
   process.argv.indexOf('--unsafe-mode') >= 0 &&
