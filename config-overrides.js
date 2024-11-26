12c29b07f9d28fab2129cc2c454281ae06e0dad2
André Abadesso <andre.abadesso@gmail.com>
2024-11-26 12:21:04 -0300

    feat: added lockdown and config overrides adding ses

--- config-overrides.js
+++ config-overrides.js
@@ -2,40 +2,52 @@ const webpack = require('webpack');
 const LavaMoatPlugin = require('@lavamoat/webpack')
 
 module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        url: require.resolve('url'),
-        fs: require.resolve('fs'),
-        assert: require.resolve('assert'),
-        crypto: require.resolve('crypto-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        buffer: require.resolve('buffer'),
-        stream: require.resolve('stream-browserify'),
-        path: require.resolve('path-browserify'),
-        vm: require.resolve('vm-browserify'),
-    };
+  config.optimization = {
+    ...config.optimization,
+    minimize: false
+  };
 
-    config.plugins = [
-        ...config.plugins,
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-            Buffer: ['buffer', 'Buffer'],
-        }),
-        new webpack.DefinePlugin({
-            'process.env': JSON.stringify(process.env),
-        }),
-        new LavaMoatPlugin({
-          writeAutoConfig: true,
-        })
-    ];
+  config.resolve.fallback = {
+    url: require.resolve('url'),
+    fs: require.resolve('fs'),
+    assert: require.resolve('assert'),
+    crypto: require.resolve('crypto-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    buffer: require.resolve('buffer'),
+    stream: require.resolve('stream-browserify'),
+    path: require.resolve('path-browserify'),
+    vm: require.resolve('vm-browserify'),
+  };
 
-    config.module.rules.push({
-        test: /\.m?js/,
-        resolve: {
-            fullySpecified: false
-        }
-    });
+  config.plugins = [
+    new LavaMoatPlugin({
+      writeAutoConfig: true,
+      inlineLockdown: /index\.js/,
+      lockdown: {
+        consoleTaming: 'unsafe',
+        errorTrapping: 'none',
+        unhandledRejectionTrapping: 'none',
+        overrideTaming: 'moderate',
+      }
+    }),
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ];
 
-    return config;
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: {
+      fullySpecified: false
+    }
+  });
+
+  return config;
 }
