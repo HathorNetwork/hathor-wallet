9ed2637856b21fb5bc5f9fd81417427d2fc1f6a3
tuliomir <tulio.mir@gmail.com>
2024-12-06 08:41:44 -0300

    chore: diff image folders

---
+++ cypress-image-diff.config.js
@@ -0,0 +1,11 @@
+/**
+ * Configurations for the visual regression plugin
+ * @see https://cypress.visual-image-diff.dev/getting-started/custom-config-file
+ */
+const config = {
+  ROOT_DIR: 'cypress',
+  REPORT_DIR: 'visual-test/reports',
+  SCREENSHOTS_DIR: 'visual-test/screenshots'
+};
+
+module.exports = config;
