3476894c3b528cb022eef5fadf05adc62150435e
André Abadesso <andre.abadesso@gmail.com>
2024-12-16 11:31:03 -0300

    chore: lavamoat policy overrides working

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
 const LavaMoatPlugin = require('@lavamoat/webpack')
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function override(config, env) {
   config.optimization = {
     ...config.optimization,
-    minimize: false
+    minimize: false,
+    concatenateModules: false
   };
 
   config.resolve.fallback = {
@@ -21,26 +24,93 @@ module.exports = function override(config, env) {
     vm: require.resolve('vm-browserify'),
   };
 
-  config.plugins = [
-    new LavaMoatPlugin({
-      writeAutoConfig: true,
-      inlineLockdown: /index\.js/,
-      lockdown: {
-        consoleTaming: 'unsafe',
-        errorTrapping: 'none',
-        unhandledRejectionTrapping: 'none',
-        overrideTaming: 'moderate',
-      }
-    }),
-    ...config.plugins,
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
-  ];
+  // Read the generated policy file
+  let policy = {};
+  const policyPath = path.join(__dirname, 'lavamoat', 'webpack');
+  const policyFile = path.join(policyPath, 'policy.json');
+  
+  try {
+    policy = JSON.parse(fs.readFileSync(policyFile, 'utf8'));
+  } catch (err) {
+    console.warn('No existing LavaMoat policy found, will generate one');
+  }
+
+  // Add custom policies
+  /* if (policy.resources) {
+    // WalletConnect policy
+    if (!policy.resources['@walletconnect/core']) {
+      policy.resources['@walletconnect/core'] = {
+        globals: {
+          ArrayBuffer: true,
+          Uint8Array: true,
+          Object: true,
+          'Object.prototype': true,
+          'Object.getPrototypeOf': true,
+          Symbol: true,
+          'Function.prototype': true
+        }
+      };
+    }
+
+    // React policy
+    if (!policy.resources['react']) {
+      policy.resources['react'] = {
+        globals: {
+          Object: true,
+          'Object.prototype': true,
+          'Function.prototype': true
+        }
+      };
+    }
+
+    // Lodash policy
+    if (!policy.resources['lodash']) {
+      policy.resources['lodash'] = {
+        globals: {
+          Object: true,
+          'Object.prototype': true,
+          'Function.prototype': true
+        }
+      };
+    }
+  }*/
+
+  // Don't use LavaMoat when running in electron
+  if (process.env.ELECTRON_RUN === 'true') {
+    config.plugins = [
+      ...config.plugins,
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
+    ];
+  } else {
+    config.plugins = [
+      new LavaMoatPlugin({
+        generatePolicy: true,
+        HtmlWebpackPluginInterop: true,
+        readableResourceIds: true,
+        diagnosticsVerbosity: 1,
+        lockdown: {
+          consoleTaming: 'unsafe',
+          errorTrapping: 'none',
+          unhandledRejectionTrapping: 'none',
+          overrideTaming: 'severe',
+        }
+      }),
+      ...config.plugins,
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
+    ];
+  }
 
   config.module.rules.push({
     test: /\.m?js/,
