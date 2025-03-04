775e78dbc21f5510ec4d00af61aaa6a42054d503
André Abadesso <andre.abadesso@gmail.com>
2025-03-04 20:11:54 -0300

    chore: updated policy and rpc lib import

--- src/sagas/reown.js
+++ src/sagas/reown.js
@@ -32,7 +32,7 @@ import {
   SendTransactionError,
   InsufficientFundsError,
   SignMessageWithAddressError,
-} from 'hathor-rpc-handler-test';
+} from '@hathor/hathor-rpc-handler';
 import { isWalletServiceEnabled } from './wallet';
 import { ReownModalTypes } from '../components/Reown/ReownModal';
 import {
