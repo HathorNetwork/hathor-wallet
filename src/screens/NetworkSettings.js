52f8675685f6ddfe2f89fbc97ceabecf4a3a4760
André Abadesso <andre.abadesso@gmail.com>
2025-04-28 14:41:15 -0300

    fearefactor: removed unused imports

--- src/screens/NetworkSettings.js
+++ src/screens/NetworkSettings.js
@@ -5,30 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { t } from 'ttag';
-import $ from 'jquery';
-import walletUtils from '../utils/wallet';
-import helpers from '../utils/helpers';
-import ReactLoading from 'react-loading';
-import hathorLib from '@hathor/wallet-lib';
-import {
-  DEFAULT_SERVERS,
-  DEFAULT_WALLET_SERVICE_SERVERS,
-  DEFAULT_WALLET_SERVICE_WS_SERVERS,
-  NETWORK_SETTINGS,
-  NETWORK_SETTINGS_STATUS,
-  colors
-} from '../constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { GlobalModalContext, MODAL_TYPES } from '../components/GlobalModal';
-import LOCAL_STORE from '../storage';
-import { getGlobalWallet } from "../modules/wallet";
-import { useNavigate } from 'react-router-dom';
-import { networkSettingsRequestUpdate, setNetworkSettingsStatus } from "../actions";
 import BackButton from '../components/BackButton';
 import NetworkSettingsForm from '../components/NetworkSettingsForm';
-
 
 /**
  * Screen to change the settings of the network the wallet is connected to.
