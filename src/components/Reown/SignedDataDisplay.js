a885dcb3409321ef0fd990b305f4ccd36c3522b9
André Abadesso <andre.abadesso@gmail.com>
2025-06-30 14:37:38 -0300

    refactor: localized strings for SignedDataDisplay component

--- src/components/Reown/SignedDataDisplay.js
+++ src/components/Reown/SignedDataDisplay.js
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import { t } from 'ttag';
 
 /**
  * Component for displaying signed data with value and signature
@@ -15,7 +16,7 @@ export const SignedDataDisplay = ({ value }) => {
     <div className="card">
       <div className="card-body p-3">
         <div className="mb-3">
-          <strong className="d-block mb-2">Value</strong>
+          <strong className="d-block mb-2">{t`Value`}</strong>
           <div className="p-2 bg-light rounded">
             {Array.isArray(value.value) ? (
               <ul className="mb-0 pl-3">
@@ -30,7 +31,7 @@ export const SignedDataDisplay = ({ value }) => {
         </div>
         
         <div className="mb-0">
-          <strong className="d-block mb-2">Signature</strong>
+          <strong className="d-block mb-2">{t`Signature`}</strong>
           <div className="d-flex align-items-start">
             <div 
               className="text-monospace flex-grow-1 p-2 bg-light rounded" 
@@ -41,7 +42,7 @@ export const SignedDataDisplay = ({ value }) => {
             <button
               className="btn btn-link btn-sm p-1 ml-2"
               onClick={() => navigator.clipboard.writeText(value.signature)}
-              title="Copy signature"
+              title={t`Copy signature`}
             >
               <i className="fa fa-copy"></i>
             </button>
