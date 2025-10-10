33d18634f8d4581c1447223db427c2beade150d8
Pedro Ferreira <pedro@hathor.network>
2025-10-10 13:08:35 -0300

    feat: wip: use new rpc lib tokenDetails data for reown send tx request

--- src/components/Reown/TransactionFeedbackModal.js
+++ src/components/Reown/TransactionFeedbackModal.js
@@ -7,8 +7,10 @@
 
 import React from 'react';
 import { t } from 'ttag';
-import { types } from '../../actions';
+import { useSelector, useDispatch } from 'react-redux';
+import { types, unregisteredTokensClean } from '../../actions';
 import { FeedbackModal } from './FeedbackModal';
+import tokens from '../../utils/tokens';
 
 export const MODAL_ID = 'transactionFeedbackModal';
 
@@ -17,12 +19,78 @@ export const MODAL_ID = 'transactionFeedbackModal';
  * Shows loading, success or error message and provides retry option on failure
  */
 export function TransactionFeedbackModal({ isError, isLoading = true, errorMessage, onClose, manageDomLifecycle }) {
+  const dispatch = useDispatch();
+  const unregisteredTokens = useSelector((state) => state.unregisteredTokens);
+
+  // Get unregistered tokens from tokensMap
+  const unregisteredTokensList = Object.values(unregisteredTokens.tokensMap || {});
+
+  const hasUnregisteredTokens = !isLoading && !isError && unregisteredTokensList.length > 0;
+
+  const handleRegisterTokens = () => {
+    // Register all unregistered tokens
+    unregisteredTokensList.forEach(token => {
+      tokens.addToken(token.uid, token.name, token.symbol);
+    });
+
+    // Clean unregistered tokens state
+    dispatch(unregisteredTokensClean());
+    onClose();
+  };
+
+  const handleClose = () => {
+    // Clean unregistered tokens state
+    dispatch(unregisteredTokensClean());
+    onClose();
+  };
+
+  const renderUnregisteredTokensComponent = () => {
+    if (!hasUnregisteredTokens) return null;
+
+    const many = unregisteredTokensList.length > 1;
+    const message = many
+      ? t`There are ${unregisteredTokensList.length} unregistered tokens in this transaction. Do you want to register them?`
+      : t`There is 1 unregistered token in this transaction. Do you want to register it?`;
+
+    return (
+      <div className="mt-3 p-3 border rounded bg-light">
+        <p className="text-muted small mb-3">
+          {message}
+        </p>
+        {unregisteredTokensList.map(token => (
+          <div key={token.uid} className="mb-2 p-2 border rounded bg-white">
+            <div className="font-weight-bold">{token.name}</div>
+            <div className="text-muted small">{token.symbol}</div>
+            <div className="text-monospace small" style={{ fontSize: '0.8rem' }}>
+              {token.uid}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const renderCustomButtons = () => {
+    if (!hasUnregisteredTokens) return null;
+
+    return (
+      <>
+        <button type="button" className="btn btn-secondary" onClick={handleClose}>
+          {t`Close`}
+        </button>
+        <button type="button" className="btn btn-hathor" onClick={handleRegisterTokens}>
+          {t`Register Tokens`}
+        </button>
+      </>
+    );
+  };
+
   return (
     <FeedbackModal
       modalId={MODAL_ID}
       isError={isError}
       isLoading={isLoading}
-      onClose={onClose}
+      onClose={hasUnregisteredTokens ? handleClose : onClose}
       manageDomLifecycle={manageDomLifecycle}
       titles={{
         loading: t`Processing Transaction`,
@@ -36,6 +104,8 @@ export function TransactionFeedbackModal({ isError, isLoading = true, errorMessa
       }}
       retryAction={types.REOWN_SEND_TX_RETRY}
       retryDismissAction={types.REOWN_SEND_TX_RETRY_DISMISS}
+      extraComponent={renderUnregisteredTokensComponent()}
+      customButtons={renderCustomButtons()}
     />
   );
 } 
--- src/sagas/reown.js
+++ src/sagas/reown.js
@@ -468,7 +468,9 @@ export function* processRequest(action) {
       } break;
       case InsufficientFundsError:
       case SendTransactionError: {
+				console.log('ERRORRRRRR', e)
         yield put(setSendTxStatusFailed());
+        yield put(unregisteredTokensClean());
         yield put(showGlobalModal(MODAL_TYPES.TRANSACTION_FEEDBACK, {
           isLoading: false,
           isError: true,
@@ -546,6 +548,10 @@ const promptHandler = (dispatch) => (request, requestMetadata) =>
     switch (request.type) {
       case TriggerTypes.SendTransactionConfirmationPrompt: {
         const sendTransactionResponseTemplate = (accepted) => () => {
+          if (!accepted) {
+            // We must clean the unregistered tokens if the user rejected the prompt
+            dispatch(unregisteredTokensClean());
+          }
           dispatch(hideGlobalModal());
           resolve({
             type: TriggerResponseTypes.SendTransactionConfirmationResponse,
